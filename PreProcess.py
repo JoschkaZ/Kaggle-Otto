44b4c40a23f880fa468a8a19c71f529a84242275
JoschkaZ <Joschka.Ziesche15@imperial.ac.uk>
2018-06-02 16:46:14 +0200

    .

---
+++ PreProcess.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import random
+from collections import Counter
+
+data=pd.read_csv(r'C:\Data\Otto\train.csv')
+#data=pd.read_csv(r'C:\Data\Otto\test.csv')
+
+
+
+
+def add_kfold_indexes(K, Data):
+    '''
+    Inputs:
+    K - Number of folds to split into
+    Data - Pandas dataframe
+    Outputs:
+    Data - Pandas dataframe with new group indices added as column "Fold"
+    '''
+    indices = list(range(len(Data)))
+    folds = [-1]*len(Data)
+
+    k = -1
+    for rep in range(len(folds)):
+        k = (k + 1) % K
+        index_of_index = random.randint(0, len(indices)-1)
+        folds[indices[index_of_index]] = k
+        del indices[index_of_index]
+
+    #print(Counter(folds))
+    Data["Fold"] = folds
+    return Data
+
+
+
+Data = add_kfold_indexes(5,data)
+print(Data)
--- RandomForest.py
+++ RandomForest.py
@@ -1,1 +1,18 @@
-print(5+5)
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.cross_validation import train_test_split
+from PreProcess import *
+
+PATH = r'C:\Data\Otto'
+
+
+
+train=pd.read_csv(PATH + r'\train.csv')
+test=pd.read_csv(PATH + r'\test.csv')
+
+
+train = add_kfold_indexes(5,train)
+
+print(train)
