dec1da6b91159cc6a503e520cf03b57e267a56b1
JoschkaZ <Joschka.Ziesche15@imperial.ac.uk>
2018-06-03 15:05:33 +0200

    .

--- PreProcess.py
+++ PreProcess.py
@@ -9,43 +9,35 @@ add_kfold_indexes(K,Data)
 train_test_merge(train,test,label)
 cat_to_int(data, cat_to_int_cols)
 '''
-### INCLUDED FUNCTIONS ###
 
-
-
-def add_kfold_indexes(K, Data):
-    """
-    :param K:  Number of folds to split into
-    :param Data: Pandas Dataframe
-
-    :return: Pandas dataframe with new Group Indices added as column "Fold"
-    """
+def add_kfold_indexes(Data, K):
     random.seed(a=42)
     indices = list(range(len(Data)))
     folds = [-1]*len(Data)
-
     k = -1
     for rep in range(len(folds)):
         k = (k + 1) % K
         index_of_index = random.randint(0, len(indices)-1)
         folds[indices[index_of_index]] = k
         del indices[index_of_index]
-
     #print(Counter(folds))
+    pd.options.mode.chained_assignment = None
     Data["Fold"] = folds
+    pd.options.mode.chained_assignment = 'warn'
     return Data
 
 def train_test_merge(train, test, label):
     test[label] = "None"
     train["IsTrain"] = True
     test["IsTrain"] = False
-
     train_test = pd.concat([train,test])
     return(train_test)
 
 def train_test_split(train_test):
     train = train_test[train_test["IsTrain"] == True]
     test = train_test[train_test["IsTrain"] == False]
+    train.drop(["IsTrain"], axis=1)
+    test.drop(["IsTrain"], axis=1)
     return train, test
 
 def cat_to_int(data, cat_to_int_cols):
@@ -54,13 +46,9 @@ def cat_to_int(data, cat_to_int_cols):
         data[col] = data[col].apply(cats.index)
     return data
 
-def kfold_split(data, k, label):
-    x_train_train = data[data["Fold"] != k]
-    y_train_train = data[data["Fold"] != k][LABEL]
-    x_train_test = data[data["Fold"] == k]
-    y_train_test = data[data["Fold"] == k][LABEL]
-
-    x_train_train.drop([LABEL, "FOLD"], axis = 1)
-    x_train_test.drop([LABEL, "FOLD"], axis = 1)
-
-    return x_train_train, y_train_train, x_train_test, x_train_train
+def kfold_split(data, k, label, features):
+    x_train_train = data[data["Fold"] != k][features].as_matrix()
+    y_train_train = data[data["Fold"] != k][label].as_matrix()
+    x_train_test = data[data["Fold"] == k][features].as_matrix()
+    y_train_test = data[data["Fold"] == k][label].as_matrix()
+    return x_train_train, y_train_train, x_train_test, y_train_test
--- RandomForest.py
+++ RandomForest.py
@@ -3,30 +3,34 @@ import pandas as pd
 from PreProcess import *
 from sklearn.ensemble import RandomForestClassifier
 from IPython.display import display
+from sklearn.metrics import confusion_matrix,classification_report, accuracy_score
 
 #SETTINGS
 PATH = r'C:\Data\Otto'
 LABEL = 'target'
+NOT_A_FEATURE = [LABEL, 'id']
 K = 5
 CAT_TO_INT_COLS = ['target'] #categorical columns to be transformed into integers
 
+#READ DATA
 train=pd.read_csv(PATH + r'\train.csv')
 test=pd.read_csv(PATH + r'\test.csv')
-train_test = train_test_merge(train,test, LABEL)
-train_test = cat_to_int(train_test, CAT_TO_INT_COLS)
-train, test = train_test_split(train_test)
-train = add_kfold_indexes(K,train)
 
-train.to_csv(r"C:\Users\Joschka\Desktop\train.csv")
+#GET FEATURES
+features = list(train.columns.values)
+features = [x for x in features if x not in NOT_A_FEATURE]
 
-'''
+#DATA TRANSFORMATIONS
+train = cat_to_int(train, CAT_TO_INT_COLS)
+train = add_kfold_indexes(train, K)
+
+#CLASSIFIER SETTINGS
 rd= RandomForestClassifier(n_estimators=100)
 
+#LOOP OVER FOLDS
 for k in range(K):
-    x_train_train, y_train_train, x_train_test, y_train_test = kfold_split(train, k, LABEL)
-
-    display(y_train.head(5))
-    input("...")
-    rd.fit(x_train,y_train)
-print(5)
-'''
+    print('Using Fold: ', k)
+    x_train_train, y_train_train, x_train_test, y_train_test = kfold_split(train, k, LABEL, features)
+    rd.fit(x_train_train, y_train_train)
+    pred = rd.predict(x_train_test)
+    print(accuracy_score(pred, y_train_test))
